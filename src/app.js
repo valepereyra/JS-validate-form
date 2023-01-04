e1642ffe51cefdf6dce35564189fa552279715cb
Valentina Pereyra Castillo <valepereyracastillo@gmail.com>
2023-01-04 17:10:25 +0000

    empezamos proyecto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //agregamos un evento al elemento form, primero obtenemos el elemento
+  let form = document.querySelector("form");
+  //Luego manipulamos el elemento agregandole el evento submit
+  // form.addEventListener("evento",funcion que se va activar)
+  //on
+  form.addEventListener("submit", enviarDatos);
+
+  //declaramos la funcion que envia los datos del formulario
+  function enviarDatos(e) {
+    e.preventDefault(); // detiene tu comportamiento predeterminado del formulario e interpretado mi codigo
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+
+    console.log(email.value);
+    console.log(password.value);
+
+    if (email.value === "") {
+      console.log("El campo email está vacio");
+    }
+  }
 };
